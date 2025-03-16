c7ae03081ac1c99acb06590801694f82c34ca96b
Dot-Link <dotlinked101@gmail.com>
2025-03-16 21:17:26 +0800

    add validation if user name exist

--- signup.php
+++ signup.php
@@ -2,21 +2,32 @@
 session_start();
 include "db.php";
 
+$error = "";
+
 if ($_SERVER['REQUEST_METHOD'] == "POST") {
     $username = $_POST['username'];
     $password = $_POST['password'];
     $hashpass = password_hash($password, PASSWORD_DEFAULT);
 
-    $query = $conn->prepare("INSERT INTO useraccount (username, userpass) VALUES (:fname, :pw)");
-    $query->bindParam(":fname", $username);
-    // $query->bindParam(":pw", $password);
-    $query->bindParam(":pw", $hashpass);
+    // Check if the username already exists
+    $checkQuery = $conn->prepare("SELECT * FROM userAccount WHERE username = :username");
+    $checkQuery->bindParam(":username", $username);
+    $checkQuery->execute();
 
-    try {
-        $query->execute();
-        header("Location: login.php");
-    } catch (\Throwable $th) {
-        echo "Something went wrong. Please try again.!!";
+    if ($checkQuery->rowCount() > 0) {
+        $error = "Username already exists.";
+    } else {
+        $query = $conn->prepare("INSERT INTO userAccount (username, userpass) VALUES (:fname, :pw)");
+        $query->bindParam(":fname", $username);
+        $query->bindParam(":pw", $hashpass);
+
+        try {
+            $query->execute();
+            header("Location: login.php");
+            exit();
+        } catch (\Throwable $th) {
+            $error = "Something went wrong. Please try again.";
+        }
     }
 }
 ?>
@@ -30,20 +41,24 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
     <link rel="stylesheet" href="/style/login.css">
 </head>
 <body>
-    <form action="" class="container"  method="POST">
-    <div class="loginform">
-                <h1 class="login">Signup</h1>
-                <div class="line"></div>
-                <h1 class="username">Username</h1>
-                <input type="text" name="username" id="username" placeholder="Username" required>
-                <h1 class="userpass">Password</h1>
-                <input type="password" name="password" id="password" placeholder="Password" required>
-                <button class="loginbtn">Signup</button>
-                <p class="signup">
-                    Already have an Account?
-                    <a href="/login.php">Login</a>
-                </p>
-            </div>
+    <form action="" class="container" method="POST">
+        <div class="loginform">
+            <h1 class="login">Signup</h1>
+            <div class="line"></div>
+            <h1 class="username">Username</h1>
+            <input type="text" name="username" id="username" placeholder="Username" required>
+            <h1 class="userpass">Password</h1>
+            <input type="password" name="password" id="password" placeholder="Password" required>
+            <?php if (!empty($error)) {
+                        echo "<p class='error'>$error</p>";
+                    }
+            ?>
+            <button class="loginbtn">Signup</button>
+            <p class="signup">
+                Already have an Account?
+                <a href="/login.php">Login</a>
+            </p>
+        </div>
     </form>
 </body>
 </html>
